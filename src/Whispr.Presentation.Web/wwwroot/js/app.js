c89bafff2c82c26d3d34366841b71dc58f0284a4
Igor Almeida <igor.almeidadasilva13@gmail.com>
2025-11-29 16:57:01 -0300

    feat: Implementation of the color palette for backgrounds, text, and buttons.

--- src/Whispr.Presentation.Web/wwwroot/js/app.js
+++ src/Whispr.Presentation.Web/wwwroot/js/app.js
@@ -1,6 +1,18 @@
-﻿function setFocusById(elementId) {
-    var element = document.getElementById("elementId");
-    if (element) {
-        element.focus();
+window.toggleTheme = () => {
+    const html = document.getElementById('htmlRoot');
+    const currentTheme = html.getAttribute('data-bs-theme');
+
+    if (currentTheme === 'dark') {
+        html.setAttribute('data-bs-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        html.setAttribute('data-bs-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    const html = document.getElementById('htmlRoot');
+    html.setAttribute('data-bs-theme', savedTheme);
+});
