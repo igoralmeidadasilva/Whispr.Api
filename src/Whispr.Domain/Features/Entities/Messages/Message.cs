77fbafcd3153ed85dcc7af56e2df6aeb950d4269
Igor Almeida da Silva <igor.almeidadasilva13@gmail.com>
2026-07-03 20:23:02 -0300

    feat: Refactor Message entity to enforce non-null content and improve validation

--- src/Whispr.Domain/Features/Entities/Messages/Message.cs
+++ src/Whispr.Domain/Features/Entities/Messages/Message.cs
@@ -1,5 +1,6 @@
 using Whispr.Domain.Core.Interfaces;
 using Whispr.Domain.Features.Entities.Users;
+using Whispr.SharedKernel.Guard;
 
 namespace Whispr.Domain.Features.Entities.Messages;
 
@@ -7,22 +8,26 @@ public sealed class Message : Entity, IAuditable
 {
     public Guid SenderId { get; private set; } // UserId
     public User? Sender { get; private set; }
-    public string? Content { get; private set; }
+    public string Content { get; private set; } = string.Empty;
     public DateTime CreatedAtUtc { get; private set; }
     public DateTime? UpdatedAtUtc { get; private set; }
 
     public Message() {} // ORM Constructor
 
-    public Message(Guid senderId, string? content) : base()
+    public Message(Guid senderId, string content) : base()
     {
+        Ensure.NotNullOrDefault(senderId, "User ID cannot be empty.", nameof(senderId));
+        Ensure.NotEmpty(content, "Content cannot be empty.", nameof(content));
+
         SenderId = senderId;
         Content = content;
-
         CreatedAtUtc = DateTime.UtcNow;
     }
 
-    public void Update(string? content)
+    public void Update(string content)
     {
+        Ensure.NotEmpty(content, "Content cannot be empty.", nameof(content));
+        
         Content = content;
         UpdatedAtUtc = DateTime.UtcNow;
     }
